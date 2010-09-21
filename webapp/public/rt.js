2dbd43d8912f08c3902d9d7f878762db3daa3d68
Marcos Piccinini <x@nofxx.com>
2010-09-21 20:43:05 -0300

    mv webapp ../sinduino

--- webapp/public/rt.js
+++
@@ -1,7 +0,0 @@
-
- var client = new Faye.Client('http://localhost:8000/faye');
-
-var subscription = client.subscribe('/stats', function(message) {
-//  console.log(message);
-  $('#stats').append("<div>" + message["data"] + "</div>");
-});
